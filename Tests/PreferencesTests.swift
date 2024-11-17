df13585cf36e46a42ed9cf378a06de5f8ff3e436
edser <edserafica@hotmail.com>
2024-11-17 15:05:27 +0800

    Update PreferencesTests.swift

--- Tests/PreferencesTests.swift
+++ Tests/PreferencesTests.swift
@@ -26,4 +26,6 @@ class PreferencesTests: XCTestCase {
         let newPreferences = Preferences()
         XCTAssertFalse(newPreferences.cacheAerials, "Property write verified")
     }
+    
+    
 }
